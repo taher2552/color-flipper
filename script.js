fc7a53ca70d39ca40f6b5de0b0aa7351abcdce8a
taher2552 <stardom.taher@gmail.com>
2022-11-17 09:05:56 +0530

    color

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ btn1.addEventListener('click', function(){
     let green2=Math.floor(Math.random()*256);
     let blue1=Math.floor(Math.random()*256);
     let blue2=Math.floor(Math.random()*256);
-    document.body.style.background='linear-gradient(to right, rgb('+red1+','+green1+','+blue1+') , rgb('+red2+','+green2+','+blue2+'))';
+    // document.body.style.background='linear-gradient(to right, rgb('+red1+','+green1+','+blue1+') , rgb('+red2+','+green2+','+blue2+'))';
+
+    document.body.style.background='linear-gradient(274deg,  rgb('+red1+','+green1+','+blue1+') 20% , rgb('+red2+','+green2+','+blue2+') 80%)';
 
     document.getElementById('btn1').innerText='rgb('+red1+','+green1+','+blue1+') , rgb('+red2+','+green2+','+blue2+')';
 })
